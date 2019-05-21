40acd5869ae20e8add7ab1cad1794fdc5683c32d
Nathanael Anderson <nathan@master-technology.com>
2019-05-21 02:14:13 -0500

    Change log

--- app/aa-page.js
+++ app/aa-page.js
@@ -3,50 +3,50 @@ const frame = require('tns-core-modules/ui/frame');
 const pageName="aa-page";
 const navigationPage = "main-page";
 
-global.log(pageName, "loaded and parsed by JavaScript Engine (only happens once per app run)");
+global.mtlog(pageName, "loaded and parsed by JavaScript Engine (only happens once per app run)");
 
 function navedTo(args) {
-    global.log(pageName, "NavigatedTo");
+    global.mtlog(pageName, "NavigatedTo");
 }
 exports.navedTo = navedTo;
 
 function navingTo(args) {
-    global.log(pageName, "NavigatingTo");
+    global.mtlog(pageName, "NavigatingTo");
 }
 exports.navingTo = navingTo;
 
 function navedFrom(args) {
-    global.log(pageName, "NavigatedFrom");
+    global.mtlog(pageName, "NavigatedFrom");
 }
 exports.navedFrom = navedFrom;
 
 function navingFrom(args) {
-    global.log(pageName, "NavigatingFrom");
+    global.mtlog(pageName, "NavigatingFrom");
 }
 exports.navingFrom = navingFrom;
 
 function loaded(args) {
-    global.log(pageName, "loaded");
+    global.mtlog(pageName, "loaded");
 }
 exports.loaded = loaded;
 
 function unloaded(args) {
-    global.log(pageName, "unloaded");
+    global.mtlog(pageName, "unloaded");
 }
 exports.unloaded = unloaded;
 
 function shownmodal(args) {
-    global.log(pageName, "shownModal");
+    global.mtlog(pageName, "shownModal");
 }
 exports.shownmodal= shownmodal;
 
 function showingmodal(args) {
-    global.log(pageName, "showingModal");
+    global.mtlog(pageName, "showingModal");
 }
 exports.showingmodal= showingmodal;
 
 function layoutChanged(args) {
-    global.log(pageName, "layoutChanged");
+    global.mtlog(pageName, "layoutChanged");
 }
 exports.layoutChanged = layoutChanged;
 
@@ -54,20 +54,20 @@ function letsNav() {
     global.resetOrder();
     console.log("");
     console.log("----------------------------------");
-    global.log(pageName, "Attempting to navigate to "+ navigationPage);
+    global.mtlog(pageName, "Attempting to navigate to "+ navigationPage);
     frame.topmost().navigate("/"+navigationPage);
 }
 exports.letsNav = letsNav;
 
 function close() {
-    global.log(pageName, "Close Modal Callback");
+    global.mtlog(pageName, "Close Modal Callback");
 }
 
 function letShow() {
     global.resetOrder();
     console.log("");
     console.log("----------------------------------");
-    global.log(pageName, "Attempting ShowModal page "+ navigationPage);
+    global.mtlog(pageName, "Attempting ShowModal page "+ navigationPage);
     frame.topmost().currentPage.showModal("/modal-page", {closeCallback: close});
 }
 exports.letsShow = letShow;
--- app/main-page.js
+++ app/main-page.js
@@ -6,8 +6,8 @@
 const frame = require('tns-core-modules/ui/frame');
 
 let orderId = 0;
-if (!global.log) {
-    global.log = function(page, value) {
+if (!global.mtlog) {
+    global.mtlog = function(page, value) {
         orderId++;
         console.log(page.padEnd(10, " ")+" ("+orderId+"): "+value );
     };
@@ -17,50 +17,50 @@ if (!global.log) {
 const pageName="main-page";
 const navigationPage = "aa-page";
 
-global.log(pageName, "loaded and parsed by JavaScript Engine (only happens once per app run)");
+global.mtlog(pageName, "loaded and parsed by JavaScript Engine (only happens once per app run)");
 
 function navedTo(args) {
-    global.log(pageName, "NavigatedTo");
+    global.mtlog(pageName, "NavigatedTo");
 }
 exports.navedTo = navedTo;
 
 function navingTo(args) {
-    global.log(pageName, "NavigatingTo");
+    global.mtlog(pageName, "NavigatingTo");
 }
 exports.navingTo = navingTo;
 
 function navedFrom(args) {
-    global.log(pageName, "NavigatedFrom");
+    global.mtlog(pageName, "NavigatedFrom");
 }
 exports.navedFrom = navedFrom;
 
 function navingFrom(args) {
-    global.log(pageName, "NavigatingFrom");
+    global.mtlog(pageName, "NavigatingFrom");
 }
 exports.navingFrom = navingFrom;
 
 function loaded(args) {
-    global.log(pageName, "loaded");
+    global.mtlog(pageName, "loaded");
 }
 exports.loaded = loaded;
 
 function unloaded(args) {
-    global.log(pageName, "unloaded");
+    global.mtlog(pageName, "unloaded");
 }
 exports.unloaded = unloaded;
 
 function shownmodal(args) {
-    global.log(pageName, "shownModal");
+    global.mtlog(pageName, "shownModal");
 }
 exports.shownmodal= shownmodal;
 
 function showingmodal(args) {
-    global.log(pageName, "showingModal");
+    global.mtlog(pageName, "showingModal");
 }
 exports.showingmodal= showingmodal;
 
 function layoutChanged(args) {
-    global.log(pageName, "layoutChanged");
+    global.mtlog(pageName, "layoutChanged");
 }
 exports.layoutChanged = layoutChanged;
 
@@ -68,20 +68,20 @@ function letsNav() {
     global.resetOrder();
     console.log("");
     console.log("----------------------------------");
-    global.log(pageName, "Attempting to navigate to "+ navigationPage);
+    global.mtlog(pageName, "Attempting to navigate to "+ navigationPage);
     frame.topmost().navigate("/"+navigationPage);
 }
 exports.letsNav = letsNav;
 
 function close() {
-    global.log(pageName, "Close Modal Callback");
+    global.mtlog(pageName, "Close Modal Callback");
 }
 
 function letShow() {
     global.resetOrder();
     console.log("");
     console.log("----------------------------------");
-    global.log(pageName, "Attempting ShowModal page "+ navigationPage);
+    global.mtlog(pageName, "Attempting ShowModal page "+ navigationPage);
     frame.topmost().currentPage.showModal("/modal-page", {closeCallback: close});
 }
 exports.letsShow = letShow;
